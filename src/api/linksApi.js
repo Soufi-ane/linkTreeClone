b2501a398eb2302d1b43e982f5c3219686a8265a
soufiane <jabersoufiane605@gmail.com>
2024-04-20 23:43:44 +0100

    fix proxy

--- src/api/linksApi.js
+++ src/api/linksApi.js
@@ -1,4 +1,4 @@
-const URL = "https://cors-anywhere.herokuapp.com/https://link-tree-clone-1kn77ibwn-soufianes-projects-8dbc43da.vercel.app";
+const URL = "https://cors-anywhere.herokuapp.com/link-tree-clone-1kn77ibwn-soufianes-projects-8dbc43da.vercel.app";
 
 export async function addLink(token, userId, text, url, color, bg_color, radius) {
     try {
--- src/api/pagesApi.js
+++ src/api/pagesApi.js
@@ -1,4 +1,4 @@
-const URL = "https://cors-anywhere.herokuapp.com/https://link-tree-clone-1kn77ibwn-soufianes-projects-8dbc43da.vercel.app";
+const URL = "https://cors-anywhere.herokuapp.com/link-tree-clone-1kn77ibwn-soufianes-projects-8dbc43da.vercel.app";
 export async function editPage(id, font, bg, token) {
     const res = await fetch(`${URL}/editPage/${id}`, {
         method: "PATCH",
