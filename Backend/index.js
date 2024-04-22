4a5045a1c4b28d9a70a99e048eb317802630408a
soufiane <jabersoufiane605@gmail.com>
2024-04-22 13:19:04 +0100

    fix dirname

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { promisify } from "util";
 import { dirname } from "path";
+import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 app.use(express.static(`${__dirname}/dist`));
